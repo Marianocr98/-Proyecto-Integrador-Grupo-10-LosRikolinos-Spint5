f537c1c913b7c398d5346c3ac223d3f1cc6ef09a
Marcelo Olivera <olivera.m.et13@gmail.com>
2022-01-04 19:40:05 -0300

    Agregando validacion para el avatar y estilos al formulario

--- src/middlewares/validationForm.js
+++ src/middlewares/validationForm.js
@@ -2,8 +2,26 @@ const path = require('path');
 const { body } = require('express-validator');
 
 
-module.exports = [
+const validations = [
     body('usuario').notEmpty().withMessage('Debe completar este campo'),
     body('email').notEmpty().withMessage('Debe escribir un correo electrónico'),
     body('password').notEmpty().withMessage('Debe escribir una contraseña'),
+    body('confirm').notEmpty().withMessage('Debe confirmar su contraseña'),
+    body('avatar').custom((value, { req }) => {
+		let file = req.file;
+		let acceptedExtensions = ['.jpg', '.png', '.gif'];
+		
+		if (!file) {
+			throw new Error('Tienes que subir una imagen');
+		} else {
+			let fileExtension = path.extname(file.originalname);
+			if (!acceptedExtensions.includes(fileExtension)) {
+				throw new Error(`Las extensiones de archivo permitidas son ${acceptedExtensions.join(', ')}`);
+			}
+		}
+
+		return true;
+	})
 ]
+
+module.exports = validations
