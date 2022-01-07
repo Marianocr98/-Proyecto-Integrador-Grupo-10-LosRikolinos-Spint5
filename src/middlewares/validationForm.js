2d596621eb2f8ebdab1c626cc2b36f839ab98373
Mayra Gutierrez <gutierrez.charcas97@gmail.com>
2022-01-07 13:44:05 -0300

    Mayra Gutierrez - modificaciones en la validacion del registro

--- src/middlewares/validationForm.js
+++ src/middlewares/validationForm.js
@@ -3,17 +3,30 @@ const { body } = require('express-validator');
 
 
 const validations = [
-    body('fullName').notEmpty().withMessage('Debe completar este campo')
-	.bail()
+    body('fullName')
+	.notEmpty().withMessage('Debe completar este campo').bail()
 	.isLength({ min : 10}).withMessage('El usuario debe tener al menos 10 caracteres'),
-    body('email').notEmpty().withMessage('Debe escribir un correo electrónico')
-	.bail()
+    
+	body('email')
+	.notEmpty().withMessage('Debe escribir un correo electrónico').bail()
 	.isEmail().withMessage('Debe ingresar un correo válido'),
-    body('password').notEmpty().withMessage('Debe escribir una contraseña')
-	.bail()
-	.isLength({ min : 8}).withMessage('La contraseña debe tener al menos 8 caracteres'),
-    body('confirm').notEmpty().withMessage('Debe confirmar su contraseña'),
-    body('avatar').custom((value, { req }) => {
+
+    body('password')
+	.notEmpty().withMessage('Debe escribir una contraseña').bail()
+	.isLength({ min : 8}).withMessage('La contraseña debe tener al menos 8 caracteres').bail()
+	.matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/, "i").withMessage('La contraseña debe contener al menos una mayúscula, una minúscula, un número y un caracter especial').bail(),
+    
+	body('confirm')
+	.trim()
+	.notEmpty().withMessage('Debe confirmar su contraseña').bail()
+	.custom(async (confirm, {req}) => {
+		let password = req.body.password;
+		if(password !== confirm) {
+		  throw new Error("Las contraseñas deben coincidir");
+		};
+	}),
+    
+	body('avatar').custom((value, { req }) => {
 		let file = req.file;
 		let acceptedExtensions = ['.jpg', '.png', '.gif'];
 		
